ec4e75fdd6155d38b3949e51cd68f991b3b3fa13
Mick Albeck <albeckm2@gmail.com>
2025-08-24 20:50:46 +1000

    Save player stats (cm,kg) to dict of player names.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome()
 #team page on nrl.com can be accessed by a unique number eg Souths is 5000005.
 #Url is https://www.nrl.com/players?competition=111&team=500005
 team_urls = {
-    "Souths": "https://www.nrl.com/players?competition=111&team=500005",
+    "south-sydney-rabbitohs": "https://www.nrl.com/players?competition=111&team=500005",
     "Broncos": "https://www.nrl.com/players?competition=111&team=500011",
     "Bulldogs": "https://www.nrl.com/players?competition=111&team=500010",
     "Cowboys": "https://www.nrl.com/players?competition=111&team=500012",
@@ -33,7 +33,8 @@ team_urls = {
     "Warriors": "https://www.nrl.com/players?competition=111&team=500032",
     "Tigers": "https://www.nrl.com/players?competition=111&team=500023"
 }
-dict_of_teams ={}
+dict_of_teams = {}
+dict_of_players = {}
 #loop through team urls
 for team in team_urls:
     #go to team page that has all their players
@@ -45,29 +46,56 @@ for team in team_urls:
 
     list_of_players = []
     #add each player to the team list_of_players
-    #TODO: maybe here instead of creating dict of team:players, I can just use team name, player name in url and go straight to player info and scrape weight etc from there and store that under player name
     for player in player_names:
-        print(player.text.replace("\n", " "))
-        list_of_players.append(player.text.replace("\n", " "))
-
-    #add team and players to dictionary in format "team": list of players
-    dict_of_teams[team] = list_of_players
-    #end up with dictionary of all teams with lists of players
-print(dict_of_teams)
-
-    # with open("broncos.txt", "w", encoding="utf-8") as file:
-    #     for player in player_names:
-    #         print(player.text)
-    #         #program is seeing players as 'adam', then 'reynolds' then 'ben', then 'hunt', etc
-    #         file.writelines(player.text)
+        #extract string of player's name from webdriver object
+        player_name = player.text.replace("\n", " ")
+        list_of_players.append(player_name)
 
 
+    #TODO: now we have list_of_players for current team, iterate through list and go to each player's page and extract weight, height, age, image?
+    # and store in dictionary under player name (instead of dict of team: list of players)
+    # eg {"Alex Johnston": ["185 cm", "95 kg", "31", "url-of-image ????"],
+    #     "Fatty Smith": ["156 cm", 150 kg", "25", "url-of-image ????"]}
+    for player_name in list_of_players:
+        #create list to eventually go into dict_of_players
+        player_stats = []
+        #url needs player names in format name-name-name
+        player_name_for_url = player_name.replace(" ", "-")
+        print(player_name_for_url)
+        # create url to go to player's page
+        player_url = "https://www.nrl.com/players/nrl-premiership/" + team + "/" + player_name_for_url + "/"
+        print(player_url)
+        driver.get(player_url)
+
+        # TODO: format of players name in url is not always name-name-name eg Campbell Graham is not working
+
+        # search for height data
+        try:
+            player_height = driver.find_element(By.XPATH, "//*[contains(text(), ' cm')]")
+            print(player_height.text)
+            player_stats.append(player_height.text)
+        except:
+            print("no height data for this player")
+            player_stats.append("no height data for this player")
+
+        # search for weight data
+        try:
+            player_weight = driver.find_element(By.XPATH, "//*[contains(text(), ' kg')]")
+            print(player_weight.text)
+            player_stats.append(player_weight.text)
+        except:
+            print("no weight data for this player")
+            player_stats.append("no weight data for this player")
+
+        dict_of_players[player_name] = player_stats
+        print(dict_of_players)
 
 
-#TODO: how to store the dictionary of club: list_of_players without losing the info as soon as program finishes??
-#TODO or maybe now use the dictionary to access each player and scrape their page for height, weight, age and image, in this same script and store all that under player name?
+#TODO: convert cm and kg to feet/inches and stones/lbs, and store that info in player_stats as well
 
-#TODO: use csv or json to store list of players for each club
-#TODO: then use club and player's name to access their page like this https://www.nrl.com/players/nrl-premiership/broncos/payne-haas
-#TODO: get their age, weight, height with Beautiful Soup or Selenium??
+#TODO: how to store the dictionary of players without losing the info as soon as program finishes??
+
+#TODO: use csv or json to store data for each player
+
 #TODO: store this info (and a photo?) somehow so that we don't have to access nrl.com for each user request
+
